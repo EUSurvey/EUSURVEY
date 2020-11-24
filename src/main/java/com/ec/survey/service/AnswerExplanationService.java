0f6a2a2fd9e38e47a09c802af8024b1ac658394e
Sahner, Julian <julian.sahner@dialogika.de>
2020-11-24 16:17:50 +0100

    Return explanations for matrices and rating questions

--- src/main/java/com/ec/survey/service/AnswerExplanationService.java
+++ src/main/java/com/ec/survey/service/AnswerExplanationService.java
@@ -46,26 +46,31 @@ public class AnswerExplanationService extends BasicService {
 
 	@Transactional(readOnly = true)
 	public List<DelphiExplanation> getDelphiExplanations(ChoiceQuestion question) {
-		return getDelphiExplanationsInternal(Collections.singletonList(question.getUniqueId()), question.getSurvey().getIsDraft());
+		return getDelphiExplanationsInternal(Collections.singletonList(question.getUniqueId()), question.getUniqueId(), question.getSurvey().getIsDraft());
 	}
 
 	@Transactional(readOnly = true)
 	public List<DelphiExplanation> getDelphiExplanations(Matrix question) {
 		List<String> uids = question.getQuestions().stream().map(Element::getUniqueId).collect(Collectors.toList());
-		return getDelphiExplanationsInternal(uids, question.getSurvey().getIsDraft());
+		return getDelphiExplanationsInternal(uids, question.getUniqueId(), question.getSurvey().getIsDraft());
 	}
 
 	@Transactional(readOnly = true)
 	public List<DelphiExplanation> getDelphiExplanations(RatingQuestion question) {
 		List<String> uids = question.getQuestions().stream().map(Element::getUniqueId).collect(Collectors.toList());
-		return getDelphiExplanationsInternal(uids, question.getSurvey().getIsDraft());
+		return getDelphiExplanationsInternal(uids, question.getUniqueId(), question.getSurvey().getIsDraft());
 	}
 
 	@Transactional(readOnly = true)
-	protected List<DelphiExplanation> getDelphiExplanationsInternal(Collection<String> questionUids, boolean isDraft) {
-		String delphiExplanationQuery = "select a.AS_ID as `answerSetId`, ex.TEXT as `explanation`, aset.ANSWER_SET_UPDATE as `update`, a.VALUE as `value`, a.QUESTION_ID as `questionId`\n" +
+	protected List<DelphiExplanation> getDelphiExplanationsInternal(Collection<String> questionUids, String mainQuestionUid, boolean isDraft) {
+		String delphiExplanationQuery = "select a.AS_ID as `answerSetId`, COALESCE(ex.TEXT, main_explanation.TEXT) as `explanation`, aset.ANSWER_SET_UPDATE as `update`, a.VALUE as `value`, a.QUESTION_ID as `questionId`\n" +
 				"from answers a\n" +
 				"left join answers_explanations ex on a.QUESTION_UID = ex.QUESTION_UID and ex.ANSWER_SET_ID = a.AS_ID\n" +
+				"left join (\n" +
+				"    select TEXT, ANSWER_SET_ID\n" +
+				"    from answers_explanations\n" +
+				"    where QUESTION_UID = :mainQuestionUid\n" +
+				"    ) as main_explanation on a.AS_ID = main_explanation.ANSWER_SET_ID\n" +
 				"join answers_set aset on a.AS_ID = aset.ANSWER_SET_ID\n" +
 				"join surveys s on aset.SURVEY_ID = s.SURVEY_ID\n" +
 				"where a.QUESTION_UID IN :questionUids AND s.ISDRAFT = :isDraft";
@@ -75,6 +80,7 @@ public class AnswerExplanationService extends BasicService {
 		query.setResultTransformer(Transformers.aliasToBean(DelphiExplanation.class));
 		query.setParameterList("questionUids", questionUids);
 		query.setBoolean("isDraft", isDraft);
+		query.setString("mainQuestionUid", mainQuestionUid);
 
 		@SuppressWarnings("unchecked")
 		List<DelphiExplanation> results = query.list();
