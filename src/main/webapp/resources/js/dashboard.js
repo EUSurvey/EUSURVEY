d48c8158dda1661c179be2eb8509fd4b4548da2e
Sahner, Julian <julian.sahner@dialogika.de>
2022-01-04 15:05:16 +0100

    [DELPHI-715] Infix search for owner on dashboard

--- src/main/webapp/resources/js/dashboard.js
+++ src/main/webapp/resources/js/dashboard.js
@@ -369,7 +369,7 @@ function DashboardViewModel()
 			var owner = $("#owner").val().trim();
 			if (owner.length > 0)
 			{
-				params = params + "&owner=" + owner;
+				params = params + "&owner=" + encodeURIComponent(owner);
 			}
 			var language = $("#language").val().trim();
 			if (language.length > 0)
